0d70531cb39dcb1871f40547b01f71fa43cce59c
luojiajing12345 <33891877+luojiajing12345@users.noreply.github.com>
2018-07-06 18:21:40 +0800

    jq封装ajax完成

--- node-test2/main.js
+++ node-test2/main.js
@@ -1,11 +1,24 @@
 window.jQuery = function(){}
 window.jQuery.ajax = function(options) {
-    let url = options.url
+    let url
+    if(arguments.length ===1 ){
+        url = options.url
+    }else if(arguments ===2){
+        url =arguments[0]
+        options = arguments[1]
+    }
     let method = options.method
     let body = options.body
     let successFn = options.successFn
     let failFn = options.failFn
+    let headers = options.headers
+
     let request = new XMLHttpRequest()
+    request.open(method, url)
+    for(let key in headers){
+        let value = headers[key]
+        request.setRequestHeader(key,value)
+    }
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
             if (request.status >= 200 && request.status < 300) {
@@ -15,17 +28,31 @@ window.jQuery.ajax = function(options) {
             }
         }
     }
-    request.open(method, url)
+
     request.send(body)
 }
  
+
+ function f1(responseText) {}
+ function f2(responseText) {}
+
  myButton.addEventListener('click',(e)=>{
-     let obj = {
+     window.jQuery.ajax({
          url:'/xxx',
-         method:'get',
+         method:'POST',
          body:'hi',
-         successFn:()=>{},
-         failFn:()=>{}
-     }
-     window.jQuery.ajax(obj)
+         headers:{
+             'content-type':'application/x-www-from-urlencoded',
+             'frank':'18'
+         }
+         successFn:(x)=>{
+             f1.call(undefined,x)
+             f2.call(undefined,x)
+         },
+         failFn:(x)=>{
+             console.log(x)
+             console.log(x.status)
+             console.log(x.responseText)
+         }
+     })
  })
